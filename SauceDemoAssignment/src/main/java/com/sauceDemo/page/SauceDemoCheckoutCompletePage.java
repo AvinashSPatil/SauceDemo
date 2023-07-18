cf972d479a061cf6ade544d26fa5d1c17dc83f8a
AvinashPatil <49275988+AvinashSPatil@users.noreply.github.com>
2023-07-18 12:05:01 +0530

    Add files via upload

---
+++ SauceDemoAssignment/src/main/java/com/sauceDemo/page/SauceDemoCheckoutCompletePage.java
@@ -0,0 +1,8 @@
+package com.sauceDemo.page;
+
+public class SauceDemoCheckoutCompletePage {
+
+    public static String CHECKOUT_COMPLETE_URL = "https://www.saucedemo.com/checkout-complete.html";
+
+
+}
